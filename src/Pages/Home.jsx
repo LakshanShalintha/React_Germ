f15fa4fcbb562a14d42ed954c62d3c4322c8dc08
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-04-02 00:17:53 +0530

    add homepage

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>
+        <b>Welcome to My First Web project</b>
+      </h1>
+      
+    </>
+  )
+};
+
+export default Home;
